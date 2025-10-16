71aebf9c2b109ee33cf8037a1a1e662f99ce8b49
mayank <mayankkumars584@gmail.com>
2025-10-16 21:23:32 +0530

    completed the wallet routes

--- app/server/routes/wallet.py
+++ app/server/routes/wallet.py
@@ -32,7 +32,7 @@ async def get_wallet(id:str):
     wallet=await retrive_wallet(id)
     if wallet:
         return responseModel(wallet, "wallets data retrieved successfully")
-    return ErrorResponseModel(wallet, "Empty list returned")
+    return ErrorResponseModel(wallet,404, "Empty list returned")
 
 @router.get('/user/{user_id}',response_description='waller data retrive by user id')
 async def get_wallet(user_id:str):
@@ -41,3 +41,32 @@ async def get_wallet(user_id:str):
         return responseModel(wallet, "wallets data retrieved successfully")
     return ErrorResponseModel(wallet, "Empty list returned")
 
+@router.put('/{user_id}',response_description='updating wallet data')
+async def update_wallet_data(user_id:str,req:WalletUpdateModel):
+    req={k:v for k, v in req.dict().items() if v is not None}
+    updated_wallet=await update_wallet(user_id,req)
+    if updated_wallet:
+        return responseModel(
+            "wallet with ID: {} data update is successful".format(user_id),
+            "wallet data updated successfully",
+        )
+    return ErrorResponseModel(
+        "An error occurred",
+        404,
+        "There was an error updating the wallet data.",
+    )
+
+@router.delete('/{id}',response_description='deleting wallet data')
+async def delete_wallet_data(id:str):
+    deleted_wallet=await delete_wallet(id)
+    if deleted_wallet:
+        return responseModel(
+            "wallet with ID: {} data deleted is successful".format(id),
+            "wallet data deleted successfully",
+        )
+    
+    return ErrorResponseModel(
+        "An error occurred",
+        404,
+        "There was an error updating the user data.",
+    )
